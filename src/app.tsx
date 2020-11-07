1ee444e0385b0c43abbccbbb93a9c10fc1148a71
Brian Kotos <brian.kotos@gmail.com>
2020-11-07 17:51:29 -0500

    Add navbar.

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,21 @@
 import React, {useEffect, useState} from 'react'
-import {FormGroup, InputGroup, Card, Button, RadioGroup, Radio} from "@blueprintjs/core"
+import {
+    FormGroup,
+    InputGroup,
+    Card,
+    Button,
+    RadioGroup,
+    Radio,
+    Navbar,
+    NavbarGroup,
+    Alignment,
+    Classes
+} from "@blueprintjs/core"
 import styled from "styled-components"
 import {DateInput, DateRangePicker} from "@blueprintjs/datetime"
 import {PersonType} from "./model"
-import {getQuarantineNameForPersonType, getStartDateNameForPersonType} from "./lib/name"
-import {formatDate, getFormattedQuarantineDurationForPersonType, getQuarantineEndDate} from "./lib/date"
+import {getQuarantineNameForPersonType, getStartDateNameForPersonType} from "lib/name"
+import {formatDate, getFormattedQuarantineDurationForPersonType, getQuarantineEndDate} from "lib/date"
 
 const Container = styled.div`
     padding: 0 2rem;
@@ -80,90 +91,97 @@ export default () => {
     }, [personType])
 
     return (
-        <Container>
-            <h1>{APP_TITLE}</h1>
-            <SpacedCard>
-                <Row>
-                    <Column>
-                        <RadioGroup
-                            inline
-                            label='Who are you interviewing?'
-                            selectedValue={personType}
-                            onChange={e => setPersonType(e.currentTarget.value as PersonType)}
-                        >
-                            {
-                                Object.values(PersonType)
-                                    .map((value, i) =>
-                                        <Radio label={value} value={value} key={i} />
-                                    )
-                            }
-                        </RadioGroup>
-                    </Column>
-                    <Column>
-                        <FullWidthFormGroup
-                            label={`${getStartDateNameForPersonType(personType)} Date`}
-                        >
-                            <DateInput
-                                formatDate={() => formattedStartDate}
-                                parseDate={str => new Date(str)}
-                                maxDate={new Date()}
-                                value={startDate}
-                                onChange={
-                                    (date: Date) => setStartDate(date)
+        <>
+            <Navbar fixedToTop>
+                <NavbarGroup align={Alignment.LEFT}>
+                    <Button className={Classes.MINIMAL} text='File' />
+                </NavbarGroup>
+            </Navbar>
+            <Container>
+                <h1>{APP_TITLE}</h1>
+                <SpacedCard>
+                    <Row>
+                        <Column>
+                            <RadioGroup
+                                inline
+                                label='Who are you interviewing?'
+                                selectedValue={personType}
+                                onChange={e => setPersonType(e.currentTarget.value as PersonType)}
+                            >
+                                {
+                                    Object.values(PersonType)
+                                        .map((value, i) =>
+                                            <Radio label={value} value={value} key={i} />
+                                        )
                                 }
-                                disabled={showQuarantineInfo}
-                            />
-                        </FullWidthFormGroup>
-                    </Column>
-                </Row>
-            </SpacedCard>
-            {
-                showQuarantineInfo &&
-                <>
-                    <SpacedCard>
-                        <Row>
-                            <Column>
-                                <FormGroup label={`${quarantineName} Duration`}>
-                                    <InputGroup
-                                        value={getFormattedQuarantineDurationForPersonType(personType)}
-                                        disabled
-                                    />
-                                </FormGroup>
-                            </Column>
-                            <Column>
-                                <FormGroup label={`${quarantineName} Start Date`}>
-                                    <InputGroup value={formattedStartDate} disabled />
-                                </FormGroup>
-                            </Column>
-                            <Column>
-                                <FormGroup label={`${quarantineName} End Date`}>
-                                    <InputGroup value={formattedEndDate} disabled />
-                                </FormGroup>
-                            </Column>
-                        </Row>
-                        <Row>
-                            <Column>
-                                <DateRangePicker
-                                    shortcuts={false}
-                                    value={[ startDate, endDate ]}
+                            </RadioGroup>
+                        </Column>
+                        <Column>
+                            <FullWidthFormGroup
+                                label={`${getStartDateNameForPersonType(personType)} Date`}
+                            >
+                                <DateInput
+                                    formatDate={() => formattedStartDate}
+                                    parseDate={str => new Date(str)}
+                                    maxDate={new Date()}
+                                    value={startDate}
+                                    onChange={
+                                        (date: Date) => setStartDate(date)
+                                    }
+                                    disabled={showQuarantineInfo}
                                 />
-                            </Column>
-                        </Row>
-                    </SpacedCard>
-                    <SpacedCard>
-                        <Row>
-                            <Column>
-                                <Button large intent='primary' icon='add' onClick={reset}>
-                                    New Calculation
-                                </Button>
-                            </Column>
-                        </Row>
-                    </SpacedCard>
-                </>
-            }
-            <Attribution>
-                Created by <a href='https://kotos.io' target='_blank'>Brian Kotos</a>
-            </Attribution>
-        </Container>
+                            </FullWidthFormGroup>
+                        </Column>
+                    </Row>
+                </SpacedCard>
+                {
+                    showQuarantineInfo &&
+                    <>
+                        <SpacedCard>
+                            <Row>
+                                <Column>
+                                    <FormGroup label={`${quarantineName} Duration`}>
+                                        <InputGroup
+                                            value={getFormattedQuarantineDurationForPersonType(personType)}
+                                            disabled
+                                        />
+                                    </FormGroup>
+                                </Column>
+                                <Column>
+                                    <FormGroup label={`${quarantineName} Start Date`}>
+                                        <InputGroup value={formattedStartDate} disabled />
+                                    </FormGroup>
+                                </Column>
+                                <Column>
+                                    <FormGroup label={`${quarantineName} End Date`}>
+                                        <InputGroup value={formattedEndDate} disabled />
+                                    </FormGroup>
+                                </Column>
+                            </Row>
+                            <Row>
+                                <Column>
+                                    <DateRangePicker
+                                        shortcuts={false}
+                                        value={[ startDate, endDate ]}
+                                    />
+                                </Column>
+                            </Row>
+                        </SpacedCard>
+                        <SpacedCard>
+                            <Row>
+                                <Column>
+                                    <Button large intent='primary' icon='add' onClick={reset}>
+                                        New Calculation
+                                    </Button>
+                                </Column>
+                            </Row>
+                        </SpacedCard>
+                    </>
+                }
+                <Attribution>
+                    Created by <a href='https://kotos.io' target='_blank'>Brian Kotos</a>
+                </Attribution>
+            </Container>
+        </>
     )
 }
--- src/lib/interview.ts
+++
@@ -1,5 +0,0 @@
-let _history: History
-let _sessionStorage: Storage
-
-export const setHistory = (history: History) => _history = history
-export const setSessionStorage = (sessionStorage: Storage) => _sessionStorage = sessionStorage
